385651cbd35f9098ef37b7fd7fbbbaf6edf7c5e5
Hernando <hernandogm73@gmail.com>
2018-11-22 15:14:05 +0100

    White House Pro

--- Scripts/main.js
+++ Scripts/main.js
@@ -23,12 +23,12 @@ var staty = {
 
 bigData();
 function bigData() {
-    if (window.location.pathname == "https://hernando35.github.io/senateAttendance.html" ||
-        window.location.pathname == "https://hernando35.github.io/senatePartyLoyalty.html" )
+    if (window.location.ref == "https://hernando35.github.io/senateAttendance.html" ||
+        window.location.ref == "https://hernando35.github.io/senatePartyLoyalty.html" )
      {
         getDataSenate();
-    } else if (window.location.pathname == "https://hernando35.github.io/houseAttendance.html" ||
-        window.location.pathname == "https://hernando35.github.io/housePartyLoyalty.html"  )
+    } else if (window.location.ref == "https://hernando35.github.io/houseAttendance.html" ||
+        window.location.ref == "https://hernando35.github.io/housePartyLoyalty.html"  )
     {
         return getDataHouse();
     } else {
